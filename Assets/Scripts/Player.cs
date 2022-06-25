2afc88fa4bdfc728ccde2730b2383f3b1f63cc25
Peter Gundel <mail@petergundel.de>
2022-06-25 17:37:17 +0200

    Flip player sprite depending on going left or right

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        MoveToStartPlatform();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    private void MoveToStartPlatform()
     {
         GameObject[] startingPlatforms = GameObject.FindGameObjectsWithTag("StartPlatform");
         transform.position = startingPlatforms[0].transform.position + new Vector3(0, (float)-0.25, 0);
+    }
+
+    private void FixedUpdate()
+    {
+        FlipSprite();
+    }
+
+    // Flip sprite depending on going left or right
+    private void FlipSprite()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        if (x > 0)
+            spriteRenderer.flipX = false;
+        else if (x < 0)
+            spriteRenderer.flipX = true;
     }
 }
