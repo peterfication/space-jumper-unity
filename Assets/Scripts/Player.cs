76715aebee8de00b8e6a18740434b5705988901d
Peter Gundel <mail@petergundel.de>
2022-06-25 17:31:18 +0200

    Move player automatically on start platform

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        GameObject[] startingPlatforms = GameObject.FindGameObjectsWithTag("StartPlatform");
+        transform.position = startingPlatforms[0].transform.position + new Vector3(0, (float)-0.25, 0);
     }
 }
