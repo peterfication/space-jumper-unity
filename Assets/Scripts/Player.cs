05993d5fc42ebb9a2f944c5fcf5f3e5e9aecb297
Peter Gundel <mail@petergundel.de>
2022-06-25 17:44:07 +0200

    Implement player movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     private SpriteRenderer spriteRenderer;
 
+    public bool isMoving = false;
+
     private void Start()
     {
         MoveToStartPlatform();
@@ -21,9 +23,10 @@ public class Player : MonoBehaviour
     private void FixedUpdate()
     {
         FlipSprite();
+        MovePlayer();
     }
 
-    // Flip sprite depending on going left or right
+    // Flip sprite depending on going left or right.
     private void FlipSprite()
     {
         float x = Input.GetAxisRaw("Horizontal");
@@ -31,5 +34,48 @@ public class Player : MonoBehaviour
             spriteRenderer.flipX = false;
         else if (x < 0)
             spriteRenderer.flipX = true;
+    }
+
+    // Move the player up, down, left and right depending on
+    // GetAxisRaw.
+    private void MovePlayer()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        if (!isMoving)
+        {
+            if (x == -1)
+                StartCoroutine(MovePlayerMovement(Vector3.left));
+            if (x == 1)
+                StartCoroutine(MovePlayerMovement(Vector3.right));
+            if (y == 1)
+                StartCoroutine(MovePlayerMovement(Vector3.up));
+            if (y == -1)
+                StartCoroutine(MovePlayerMovement(Vector3.down));
+        }
+    }
+
+    // Actually move the player to the specified direction
+    // in a smooth animation.
+    private IEnumerator MovePlayerMovement(Vector3 direction)
+    {
+        isMoving = true;
+
+        float timeToMove = 0.2f;
+
+        Vector3 origPos = transform.position;
+        Vector3 targetPos = origPos + direction;
+
+        float elapsedTime = 0;
+        while (elapsedTime < timeToMove)
+        {
+            transform.position = Vector3.Lerp(origPos, targetPos, (elapsedTime / timeToMove));
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        transform.position = targetPos;
+        isMoving = false;
     }
 }
